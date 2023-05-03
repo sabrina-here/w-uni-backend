d2737292b8bbc3b44319c453cb57dfad70995661
shah-here <shahrinakhan6@gmail.com>
2023-05-03 07:29:42 +0600

    m

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
